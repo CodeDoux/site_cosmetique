fb43f663a36578da9649bfef620f18910e191cb0
CodeDoux <sarrcodou98@gmail.com>
2026-06-09 15:24:45 +0100

    fix url coolify laravel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api',
-  storageUrl: 'http://localhost:8000/storage'  // ← bien présent ?
+  apiUrl: 'http://yzaupufbv2alpzwkh9kjzn8t.91.98.66.79.sslip.io/api',
+  storageUrl: 'http://yzaupufbv2alpzwkh9kjzn8t.91.98.66.79.sslip.io/storage'  // ← bien présent ?
 };
