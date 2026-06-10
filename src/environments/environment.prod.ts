446e56ea452e3479103ee2ecf25c9094602a4c1f
CodeDoux <sarrcodou98@gmail.com>
2026-06-10 17:24:14 +0100

    fix: change environment with domaine name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://yzaupufbv2alpzwkh9kjzn8t.91.98.66.79.sslip.io/api',
-  storageUrl: 'http://yzaupufbv2alpzwkh9kjzn8t.91.98.66.79.sslip.io/storage'  // ← bien présent ?
+  apiUrl: 'https://api.toulayskin.sn/api',
+  storageUrl: 'https://api.toulayskin.sn/storage'  // ← bien présent ?
 };
